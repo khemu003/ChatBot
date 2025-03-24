881fd40f6df850c2022449bbf6df7a3cd9ae5311
Khemraj Mangal <mangalkhemraj@gmail.com>
2025-03-24 11:29:47 +0530

    Added all files

---
+++ api.py
@@ -0,0 +1,14 @@
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+load_dotenv()
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+def chat_with_gemini(prompt):
+    try:
+        model = genai.GenerativeModel("gemini-1.5-flash")
+        response = model.generate_content(prompt)
+        return response.text
+    except Exception as e:
+        return str(e)
